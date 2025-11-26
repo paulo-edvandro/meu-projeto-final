0798c71b29934b38820afb3ea49b7ba317dcd251
Paulo <paulaotriz7@gmail.com>
2025-11-26 18:52:13 -0300

    html

--- app.js
+++ app.js
@@ -1,42 +1,66 @@
 const express = require("express");
-//para ler arquivos
-const fs = require("fs");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
-
-//permite uso de json
 app.use(express.json());
 
+// CONFIG SUPABASE
+const supabase = createClient(
+  "https://hvxjloyumbdhbkzbcpqu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2eGpsb3l1bWJkaGJremJjcHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzkwODQsImV4cCI6MjA3OTc1NTA4NH0.I8rPduAf7GiUHZ_NnTRwth1Lj-gkeBDNguly9ktYSJs"
+);
 
-app.get("/users", (req, res) => {
-  fs.readFile(__dirname + "/users.json", "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: "Erro ao ler arquivo." });
-    }
-    res.end(data); // devolve o JSON inteiro
-  });
+// LISTAR TODOS OS PRODUTOS
+app.get("/produtos", async (req, res) => {
+  const { data, error } = await supabase.from("produtos").select("*");
+  if (error) return res.status(400).json(error);
+  res.json(data);
 });
 
-app.get("/users/:id", (req, res) => {
-  fs.readFile(__dirname + "/users.json", "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: "Erro ao ler arquivo." });
-    }
-
-    const users = JSON.parse(data);
-    const user = users["user" + req.params.id];
-
-    if (!user) {
-      return res.status(404).json({ message: "Usuário não encontrado." });
-    }
-
-    res.json(user);
-  });
+// LISTAR PRODUTOS POR ID
+app.get("/produtos/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("produtos")
+    .select("*")
+    .eq("id", req.params.id)
+    .single();
+  if (error) return res.status(400).json(error);
+  res.json(data);
 });
 
-const PORT = process.env.PORT || 8080;
+// CRIAR UM PRODUTO
+app.post("/produtos", async (req, res) => {
+  const { nome, descricao, preco } = req.body;
+  const { data, error } = await supabase
+    .from("produtos")
+    .insert([{ nome, descricao, preco }]);
+  if (error) return res.status(400).json(error);
+  res.json(data);
+});
 
+// ATUALIZAR UM PRODUTO
+app.put("/produtos/:id", async (req, res) => {
+  const { nome, descricao, preco } = req.body;
+  const { data, error } = await supabase
+    .from("produtos")
+    .update({ nome, descricao, preco })
+    .eq("id", req.params.id);
+  if (error) return res.status(400).json(error);
+  res.json(data);
+});
+
+// DELETAR UM PRODUTO
+app.delete("/produtos/:id", async (req, res) => {
+  const { error } = await supabase
+    .from("produtos")
+    .delete()
+    .eq("id", req.params.id);
+  if (error) return res.status(400).json(error);
+
+  res.json({ message: "Deletado com sucesso" });
+});
+
+const PORT = 8080;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`REST API rodando na porta ${PORT}`);
+  console.log("API rodando na porta " + PORT);
 });
-
