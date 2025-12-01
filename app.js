2d0d394837a34ce4d72f4b65fff6978c03ec9c72
Paulo <paulaotriz7@gmail.com>
2025-12-01 13:11:28 -0300

    add correção

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const { createClient } = require("@supabase/supabase-js");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 // CONFIG SUPABASE
 const supabase = createClient(
